c890d0d86bf8137d378434c830c98d66a13a229e
Brendan Luu <brendan@vumble.com>
2015-09-06 07:24:45 -0400

    Getting location

--- MOV/ViewController.swift
+++ MOV/ViewController.swift
@@ -27,42 +27,13 @@ class ViewController: UIViewController {
     var screenWidth = (UIScreen.mainScreen().bounds.width)
     var screenHeight = (UIScreen.mainScreen().bounds.height)
     
+    var leftView = UIView()
+    var centerView = UIView()
+    var rightView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        screenWidth = screenWidth - rewindButton.frame.size.width
-        let transform = CGAffineTransformMakeScale;
-        progressBar.transform = CGAffineTransformScale(progressBar.transform, 1.9, 50)
         
-        let path = NSBundle.mainBundle().pathForResource("dronefish", ofType:"mp4")
-        let url = NSURL.fileURLWithPath(path!)
-        
-        self.moviePlayer = MPMoviePlayerController(contentURL: url)
-        if let player = self.moviePlayer {
-            player.view.frame = CGRect(x: 0, y: 0, width: 550, height: self.view.frame.size.height-50)
-            globalPlayer = player
-            player.view.sizeToFit()
-            player.scalingMode = MPMovieScalingMode.Fill
-            player.controlStyle = MPMovieControlStyle.None
-            player.movieSourceType = MPMovieSourceType.File
-            player.play()
-            self.view.addSubview(player.view)
-            var timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: "watcher:", userInfo: player, repeats: false)
-        }
-        var DynamicView=UIView(frame: CGRectMake(0, 0, screenWidth/3, screenHeight))
-
-        DynamicView.backgroundColor=UIColor.blackColor()
-        
-        self.view.addSubview(DynamicView)
-        var DynamicView2=UIView(frame: CGRectMake(screenWidth/3, 0, screenWidth/3, screenHeight))
-        DynamicView2.backgroundColor=UIColor.whiteColor()
-        
-        self.view.addSubview(DynamicView2)
-        var DynamicView4=UIView(frame: rewindButton.frame)
-        DynamicView4.backgroundColor=UIColor.whiteColor()
-        self.view.addSubview(DynamicView4)
-        var DynamicView3=UIView(frame: CGRectMake(2*screenWidth/3, 0, screenWidth/3, screenHeight))
-        DynamicView3.backgroundColor=UIColor.greenColor()
-        self.view.addSubview(DynamicView3)
         
         
         // First, get the location of the touch
@@ -90,7 +61,157 @@ class ViewController: UIViewController {
         
     }
     
-    var rewinding = false
+    var centerTapSelector = UILongPressGestureRecognizer()
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        //let transform = CGAffineTransformMakeScale;
+        //progressBar.transform = CGAffineTransformScale(progressBar.transform, 1, 1)
+        
+        let path = NSBundle.mainBundle().pathForResource("dronefish", ofType:"mp4")
+        let url = NSURL.fileURLWithPath(path!)
+        
+        screenWidth = screenWidth - rewindButton.frame.size.width
+        screenHeight = screenHeight - progressBar.frame.size.height
+        
+        self.moviePlayer = MPMoviePlayerController(contentURL: url)
+        if let player = self.moviePlayer {
+            player.view.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+            globalPlayer = player
+            player.view.sizeToFit()
+            player.scalingMode = MPMovieScalingMode.Fill
+            player.controlStyle = MPMovieControlStyle.None
+            player.movieSourceType = MPMovieSourceType.File
+            player.play()
+            self.view.addSubview(player.view)
+            var timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: "watcher:", userInfo: player, repeats: false)
+        }
+        
+        
+        leftView=UIView(frame: CGRectMake(0, 0, screenWidth/3, screenHeight))
+        let leftTapSelector = UILongPressGestureRecognizer(target: self, action: "leftTap:")
+        //leftTapSelector.minimumPressDuration = 1
+        leftView.addGestureRecognizer(leftTapSelector)
+        leftView.backgroundColor=UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        self.view.addSubview(leftView)
+        
+        centerView=UIView(frame: CGRectMake(screenWidth/3, 0, screenWidth/3, screenHeight))
+        centerTapSelector = UILongPressGestureRecognizer(target: self, action: "centerTap:")
+        centerTapSelector.minimumPressDuration = 0
+        centerView.addGestureRecognizer(centerTapSelector)
+        //let centerReleaseSelector = UITapGestureRecognizer(target: self, action: Selector("centerTap"))
+        //centerView.addGestureRecognizer(centerTapSelector)
+        self.view.addSubview(centerView)
+
+        rightView=UIView(frame: CGRectMake(2*screenWidth/3, 0, screenWidth/3, screenHeight))
+        let rightTapSelector = UILongPressGestureRecognizer(target: self, action: "rightTap:")
+        //rightTapSelector.minimumPressDuration = 1
+        rightView.addGestureRecognizer(rightTapSelector)
+        rightView.backgroundColor=UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        self.view.addSubview(rightView)
+    
+        //centerTapSelector.delegate = self
+        
+        //let centerTapSelector = UIGestureRecognizer(self, action: Selector("centerTap:"), forControlEvents: UIControlEvents.TouchDown)
+        
+        
+        
+        //centerView.userInteractionEnabled = true
+        
+        
+
+
+        
+    }
+    
+    //var tracking = false!
+    func leftTap(gestureRecognizer:UIGestureRecognizer) {
+        if(gestureRecognizer.state != UIGestureRecognizerState.Ended)
+        {
+            
+            /*let pos = gestureRecognizer.locationInView(leftView)
+            let x = pos.x
+            let y = pos.y
+            print("(")
+            print(String(stringInterpolationSegment: x) + ", ")
+            print(String(stringInterpolationSegment: y) + ")")*/
+            
+        }
+    }
+    //var tracking = false!
+    func centerTap(gestureRecognizer:UIGestureRecognizer) {
+        if(gestureRecognizer.state != UIGestureRecognizerState.Ended)
+        {
+            let pos = gestureRecognizer.locationInView(centerView)
+            let x = pos.x
+            let y = pos.y
+            print("(")
+            print(String(stringInterpolationSegment: x) + ", ")
+            print(String(stringInterpolationSegment: y) + ")")
+            println()
+            
+            
+            
+        }
+    }
+    //var tracking = false!
+    func rightTap(gestureRecognizer:UIGestureRecognizer) {
+        if(gestureRecognizer.state != UIGestureRecognizerState.Ended)
+        {
+            /*let pos = gestureRecognizer.locationInView(rightView)
+            let x = pos.x
+            let y = pos.y
+            print("(")
+            print(String(stringInterpolationSegment: x) + ", ")
+            print(String(stringInterpolationSegment: y) + ")")
+            println()*/
+        }
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        for touch : AnyObject in touches {
+        var startPoint = touch.locationInView(self.view)
+        
+        let x = startPoint.x
+        let y = startPoint.y
+        print("(")
+        print(String(stringInterpolationSegment: x) + ", ")
+        print(String(stringInterpolationSegment: y) + ")")
+        println()
+        }
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        for touch : AnyObject in touches {
+            var touchLocation = touch.locationInView(self.view)
+            let x = touchLocation.x
+            let y = touchLocation.y
+            print("(")
+            print(x)
+            print(", ")
+            print(y)
+            print(")")
+            println()
+            
+            if()
+        }
+    }
+    
+    //override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        for touch : AnyObject in touches {
+        var endPoint = touch.locationInView(self.view)
+        let x = endPoint.x
+        let y = endPoint.y
+        print("(")
+        print(x)
+        print(", ")
+        print(y)
+            print(")")
+            println()
+        }
+    }
     
     @IBAction func rewind(sender: AnyObject) {
         globalPlayer.pause()
@@ -110,7 +231,14 @@ class ViewController: UIViewController {
     }*/
     
     
-    
+    /*@IBAction func centerTap(sender: AnyObject)
+    {
+        
+        globalPlayer.stop()
+        //rewinding = false
+        //globalPlayer.endSeeking()
+        //globalPlayer.pause()
+    }*/
     
     
     @IBAction func touch_up_play(sender: AnyObject)
